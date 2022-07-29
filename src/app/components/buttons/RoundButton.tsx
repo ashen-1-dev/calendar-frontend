83624d658382ca37970595ea6a41a1d761cd3dab
Nikita Lepeykin <lepeykin@arview.pro>
2022-07-29 13:49:16 +0700

    refactor(button): add variables to button style

--- src/app/components/buttons/RoundButton.tsx
+++ src/app/components/buttons/RoundButton.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Colors } from '../../../styles/colors';
 import { ReactComponent as PlusSvg } from './assets/plus.svg';
 import Button from './Button';
-import classNames from 'classnames';
 
 const RoundButton: React.FC = () => {
   return (
