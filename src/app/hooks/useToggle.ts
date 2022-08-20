25ee96c98383a8b9b746ca17e2571876a2220fd0
Nikita Lepeykin <lepeykin@arview.pro>
2022-08-20 13:15:13 +0700

    refactor(Select): refactor select

--- src/app/hooks/useToggle.ts
+++ src/app/hooks/useToggle.ts
@@ -3,7 +3,7 @@ import { useCallback, useState } from 'react';
 export const useToggle = (
   initialState: boolean = false,
 ): [state: boolean, toggle: () => void] => {
-  const [state, setState] = useState(initialState);
+  const [state, setState] = useState<boolean>(initialState);
 
   const toggle = useCallback(() => setState(state => !state), []);
 
